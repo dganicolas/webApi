55c983af1efed07a555f35170e71949a2bcf2f7e
dganicolas <dganicolas@rafaelalberti.es>
2025-03-08 07:55:09 +0100

    xd era un put o%

--- webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16f3a49b5f470aea20871c2463a7b29e4a231cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d833367446520a70aec5d7ee89093ab8a4767b9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
