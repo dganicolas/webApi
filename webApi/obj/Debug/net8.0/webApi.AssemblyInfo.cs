637bbaa212c9917e77dc4f2a8ac68d06d3947d2c
dganicolas <dganicolas@rafaelalberti.es>
2025-03-09 07:55:43 +0100

    prueba

--- webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746f02c107e4131e93efa2584eba18e9d8cdb9c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56ddbc70b218b137e60046dc6e7194a8f71a589")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
